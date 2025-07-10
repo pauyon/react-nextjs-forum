71c132dfe4f6210fc223291e60c2cb1b522e0972
Paulino Auyon <pauyon@jgboswell.com>
2025-07-10 16:07:20 -0700

    Refactor header so home page becomes a static route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,7 @@
-import { Button } from '@nextui-org/react';
-import * as actions from '@/actions';
-import { auth } from '@/auth';
-import Profile from '@/components/profile';
-
-export default async function Home() {
-  const session = await auth(); // server component way
-
+export default function Home() {
   return (
    <div>
-    <form action={actions.signIn}>
-      <Button type='submit'>Sign In</Button>
-    </form>
-    <form action={actions.signOut}>
-      <Button type='submit'>Sign Out</Button>
-    </form>
-
-    { 
-      session?.user ? <div>Signed In</div> : <div>Signed Out</div>
-    }
-
-    <Profile/>
+      Home Page
    </div> 
   )
 }
