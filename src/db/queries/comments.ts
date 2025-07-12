8b7d7962738c5226f65b5d6ce7d934dc379cc6d7
Paulino Auyon <pauyon@jgboswell.com>
2025-07-11 17:58:05 -0700

    Refactor using memoization

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,12 +1,13 @@
 import type { Comment } from '@prisma/client'
+import { cache } from 'react';
 import { db } from '@/db';
 
 export type CommentWithAuthor = Comment & {  
     user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentsByPostId(postId: string)
-: Promise<CommentWithAuthor[]> {
+export const fetchCommentsByPostId = cache((postId: string)
+: Promise<CommentWithAuthor[]> => {
     return db.comment.findMany({
         where: { postId },
         include: {
@@ -18,4 +19,4 @@ export function fetchCommentsByPostId(postId: string)
             },
         },
     });
-}
+});
